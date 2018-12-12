8f2687db09b6159fbc4bdcba8e2592b21a9d4f0a
Kseniya <kseniya.utarbekova@gmail.com>
2018-12-12 18:49:46 +0600

    Added Palindrome and Sequence Task Solutions

---
+++ Palindrome/src/main/palindrome/PalindromeRunner.java
@@ -0,0 +1,25 @@
+package src.main.palindrome;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class PalindromeRunner {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String word;
+        Boolean isPalindrome = true;
+
+        while (isPalindrome){
+            word = reader.readLine();
+            StringBuilder revertedWord = new StringBuilder(word);
+            revertedWord = revertedWord.reverse();
+            if(word.equals(revertedWord.toString())){
+                System.out.println(revertedWord + " is a palindrome");
+            } else {
+                isPalindrome = false;
+                System.out.println("the word isn't a palindrome");
+            }
+        }
+    }
+}
---
+++ SequenceRunner/src/main/sequence/SequenceRunner.java
@@ -0,0 +1,94 @@
+package src.main.sequence;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SequenceRunner {
+    static String string;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String sequenceOne = reader.readLine();
+        String sequenceTwo = reader.readLine();
+
+        // First, check if the sequence contains only 0 and 1.
+        if (!sequenceOne.matches("[01]+") || (!sequenceTwo.matches("[01]+"))) {
+            System.out.println("The sequence shall contain 0 and 1 only.");
+        } else if (sequenceOne.length() == sequenceTwo.length()) {
+            if (isFirstCharsEqual(sequenceOne, sequenceTwo)) {
+                if (isSequenceEqual(sequenceOne, sequenceTwo)) {
+                    System.out.println("The sequences are equal");
+                } else {
+                    System.out.println("The sequences are not equal");
+                }
+            } else {
+                System.out.println("The first symbol in sequences is different");
+            }
+        } else {
+            System.out.println("The length of sequences is different.");
+        }
+    }
+
+            //The method checks if the number of zeroes in both sequences is the same
+//        public static boolean isEqualQuantity(String one, String two) {
+//        int firstSequenceZeroes = calculateZeros(one);
+//        int secondSequenceZeroes = calculateZeros(two);
+//        if (firstSequenceZeroes == secondSequenceZeroes) {
+//            return true;
+//        }
+//        return false;
+//    }
+        // The method calculates the number of zeroes in a sequence.
+//    public static int calculateZeros(String sequence) {
+//        char[] chars = sequence.toCharArray();
+//        int zeros = 0;
+//        for (char ch : chars) {
+//            if (ch == '0') {
+//                zeros++;
+//            }
+//        }
+//        return zeros;
+//    }
+
+    // The method checks if the first symbol in sequences is the same.
+    public static boolean isFirstCharsEqual(String one, String two) {
+        char[] charsOne = one.toCharArray();
+        char[] charsTwo = two.toCharArray();
+        if (charsOne[0] == charsTwo[0]) {
+            return true;
+        }
+        return false;
+    }
+
+    //The method replaces substings for reversed ones.
+    public static String replaceChars(String sequence) {
+        string = new String(sequence);
+        Pattern pattern = Pattern.compile("1{1}0{1,}1{2,}");
+        Matcher matcher = pattern.matcher(string);
+        while (matcher.find()) {
+            StringBuilder subsb = new StringBuilder();
+            String subsequence = matcher.group();
+            subsb.append(subsequence);
+            subsb.reverse();
+            string = string.replaceFirst(subsequence, subsb.toString());
+            replaceChars(string);
+        }
+
+        return string;
+    }
+
+    // The method compares two sequences received after replacement of substrings.
+    public static boolean isSequenceEqual(String one, String two) {
+        String sequenceOne = replaceChars(one);
+        System.out.println(sequenceOne);
+        String sequenceTwo = replaceChars(two);
+        System.out.println(sequenceTwo);
+        if (sequenceOne.equals(sequenceTwo)) {
+            return true;
+        }
+        return false;
+    }
+}
